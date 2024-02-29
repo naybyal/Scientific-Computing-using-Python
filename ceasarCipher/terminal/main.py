817b52c7cc34d30cb977235819624df7f269f0f4
naybyal <officiallynabiel@gmail.com>
2024-02-29 20:22:29 +0530

    Ceasar Cipher

---
+++ ceasarCipher/terminal/main.py
@@ -0,0 +1,33 @@
+global distance
+
+def ceasarEncrypt(message):
+    encryptedMessage = ""
+    for ch in message:
+        ordinalValue = ord(ch)
+        cipherValue = ordinalValue + distance
+        if cipherValue > ord('z'):
+            cipherValue = ord('a') + distance - (ord('z') - ordinalValue + 1)
+
+        encryptedMessage += chr(cipherValue)
+    return encryptedMessage
+
+
+def ceasarDecrypt(encryptedMessage):
+    message = ""
+    for ch in encryptedMessage:
+        cipherValue = ord(ch)
+        originalValue = cipherValue - distance
+        if originalValue < ord('a'):
+            originalValue = ord('z') - distance + (ord('z') - cipherValue + 1)
+        message += chr(originalValue)
+    return message
+
+
+message = input('Reading the message : ')
+distance = int(input("Reading the distance (k) : "))
+
+encrypted = ceasarEncrypt(message)
+print(encrypted)
+
+decrypted = ceasarDecrypt(encrypted)
+print(decrypted)
