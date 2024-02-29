b3075c2c89cb7e8ce2fe768f7feebe604b54d43d
naybyal <officiallynabiel@gmail.com>
2024-02-29 20:49:24 +0530

    Ceasar Cipher GUI

--- ceasarCipher/ceasarCipherGUI/main.py
+++ ceasarCipher/ceasarCipherGUI/main.py
@@ -1,14 +1,16 @@
+from tkinter import *
+from tkinter import ttk
 global distance
-
 
 def ceasarEncrypt(message):
     encryptedMessage = ""
+
     for ch in message:
         ordinalValue = ord(ch)
         cipherValue = ordinalValue + distance
+
         if cipherValue > ord('z'):
             cipherValue = ord('a') + distance - (ord('z') - ordinalValue + 1)
-
         encryptedMessage += chr(cipherValue)
     return encryptedMessage
 
@@ -22,4 +24,17 @@ def ceasarDecrypt(encryptedMessage):
             originalValue = ord('z') - distance + (ord('z') - cipherValue + 1)
         message += chr(originalValue)
     return message
+
+
+
+root = Tk()
+root.title("Ceasar Cipher!")
+
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+
+ttk.Label(frame, text='Ceasar Cipher!').grid(column=0, row=0)
+ttk.Button(frame, text='Encrypt', command=ceasarEncrypt).grid(column=1, row=0)
+
+root.mainloop()
 
