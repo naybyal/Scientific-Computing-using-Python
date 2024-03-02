3f3260aa9d2d45b04e7b2567a30ba3cd8de88e1e
naybyal <officiallynabiel@gmail.com>
2024-03-02 18:36:45 +0530

    Triangular Pattern (Alphabets)

--- Assignment 1/pattern1.py
+++ Assignment 1/pattern1.py
@@ -1,10 +1,11 @@
-rows = int(input('Row count : '))
+rows = int(input('Row count (not greater than 26) : '))
 
+if rows <= 26 and rows > 0:
 # A --ASCII VALUE--> 65
-for i in range(1, rows+1):
-    start = 65
-    for j in range(i):
-        alphabet = chr(start)
-        print(alphabet, end=" ")
-        start += 1
-    print()
+    for i in range(1, rows+1):
+        start = 65
+        for j in range(i):
+            alphabet = chr(start)
+            print(alphabet, end=" ")
+            start += 1
+        print()
