50f456788d0298a748753a8fa309a54fb263146f
naybyal <officiallynabiel@gmail.com>
2024-03-18 14:51:58 +0530

    Dictionary

--- classNotes_Mar18/mar18/dictionary.py
+++ classNotes_Mar18/mar18/dictionary.py
@@ -1,2 +1,16 @@
-phoneBook = {'Savannah' : '476', 'Rahul' : '1324'} 
-print(phoneBook)
+phoneBook = {
+    'Savannah' : '476', 
+    'Rahul' : '1324'
+}
+
+print(phoneBook.get('Rahul'))
+
+info = {}
+info['name'] = 'Sandy'
+info['occupation'] = 'Hacker'
+
+print(info.get('name'))
+
+info.pop('name')
+print(info)
+print(info.get('occupation'))
