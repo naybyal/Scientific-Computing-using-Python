1d72c46df34bf3016356c53e7f7ba5e3f3bbb9c8
naybyal <officiallynabiel@gmail.com>
2024-03-18 14:31:56 +0530

    Functions

--- classNotes_Mar18/mar18/function.py
+++ classNotes_Mar18/mar18/function.py
@@ -2,3 +2,13 @@ def square(n):
     return n**2
 
 print(square(2))
+
+def average(list):
+    sum = 0
+    for element in list:
+        sum += element
+
+    return sum/len(list)
+
+list = [1,2,3,4,5]
+print(average(list))
