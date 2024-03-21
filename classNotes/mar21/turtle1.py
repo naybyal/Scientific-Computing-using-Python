9b18fccfe89cd022a1b0a0a4b5848b94d1f3dfd7
naybyal <officiallynabiel@gmail.com>
2024-03-21 10:30:00 +0530

    First Turtle Program

--- classNotes/mar21/turtle1.py
+++ classNotes/mar21/turtle1.py
@@ -4,11 +4,16 @@ from turtle import *
 
 
 # t = Turtle()
-my_window = turtle.Screen()
-my_window.bgcolor('grey')
+my_window = Screen()
+my_window.bgcolor('black')
 t = Turtle()
-
+t.width(5)
+t.color('red')
 t.forward(75)
 t.left(90)
 t.forward(75) 
+t.left(90)
+t.forward(75)
+t.left(90)
+t.forward(90)
 t.screen.mainloop()
