17b12c993af2ef49166f9988646a55d1895f230b
naybyal <officiallynabiel@gmail.com>
2024-03-21 10:17:37 +0530

    first turtle program

---
+++ classNotes/mar21/turtle1.py
@@ -0,0 +1,14 @@
+import turtle
+from turtle import *
+
+
+
+# t = Turtle()
+my_window = turtle.Screen()
+my_window.bgcolor('grey')
+t = Turtle()
+
+t.forward(100)
+t.left(90)
+t.forward(75) 
+t.screen.mainloop()
