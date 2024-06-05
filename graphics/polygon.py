acc82a2b164d591ee4fc935943f12db667f8de72
naybyal <officiallynabiel@gmail.com>
2024-06-05 20:29:47 +0530

    revision: turtle graphics

--- graphics/polygon.py
+++ graphics/polygon.py
@@ -10,12 +10,16 @@ theta1 = 360/n
 theta2 = abs(180 - theta1)
 
 t = turtle.Turtle()
-
+t.up()
+t.goto(0,0)
 t.setheading(theta2)
+t.down()
 
 for i in range(n):
-    t.forward(200)
+    t.forward(100)
     t.right(theta1)
 
-
 turtle.Screen().mainloop()
+
+
+
